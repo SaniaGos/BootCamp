2684219c1b33fda904824252bfa53c7eae507143
Yaroslav Ersteniuk <YaroslavErsteniuk@pirscapital.com>
2023-09-11 12:25:19 +0300

    Add project files.

---
+++ BootCamp/Program.cs
@@ -0,0 +1,41 @@
+﻿using BootCamp;
+
+
+public class People
+{
+    public int Id { get; set; }
+}
+
+public class Admin : User
+{
+    public Admin() : base("Vasul")
+    {
+
+    }
+
+    public new void Foo2()
+    {
+        Console.WriteLine("I am admin!");
+    }
+
+    public override void Foo()
+    {
+        Console.WriteLine();
+    }
+}
+
+public sealed class SuperAdmin : Admin
+{
+
+}
+
+class Run
+{
+    public static void Main()
+    {
+        var role = User.GetRole();
+        
+        Console.WriteLine(role);
+    }
+
+}
---
+++ BootCamp/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp
+{
+    public abstract class User
+    {
+        public User(string userName)
+        {
+            ExternalId = string.Empty;
+            Username = userName;
+        }
+
+        public static string GetRole()
+        {
+            return "User";
+        }
+
+        public int Id { get; set; }
+
+        public string ExternalId { get; set; }
+
+        public string Username { get; set; }
+
+        public abstract void Foo();
+
+        public virtual void Foo2()
+        {
+            Console.WriteLine("I am user!");
+        }
+    }
+}
