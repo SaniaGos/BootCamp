7f6edb1d7c1638beff14f23529e9776f0c20b951
vasylnetls <vasylnetls643@gmail.com>
2023-09-18 14:50:29 +0300

    addeed romb and tree

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void UpdateBorder(int i, int middleBorder, ref int leftBorder, ref int rightBorder)
+//        {
+//            if (i < middleBorder)
+//            {
+//                leftBorder--;
+//                rightBorder++;
+//            }
+//            else
+//            {
+//                leftBorder++;
+//                rightBorder--;
+//            }
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            const int rombSize = 21;
+//            int leftBorder, rightBorder, middleBorder = rombSize / 2;
+//            leftBorder = rightBorder = middleBorder;
+//            int startX = (Console.WindowWidth - rombSize) / 2;
+
+//            for (int i = 0; i < rombSize; i++)
+//            {
+//                for (int j = 0; j < rombSize; j++)
+//                {
+//                    if (leftBorder <= j && j <= rightBorder)
+//                    {
+//                        Console.SetCursorPosition(j + startX, i);
+//                        Console.Write("*");
+//                    }
+//                }
+
+//                UpdateBorder(i, middleBorder, ref leftBorder, ref rightBorder);
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
